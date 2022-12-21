f8f330342655f632d037e1db6cc0796a15343b24
michalR <michal1995.1995@interia.pl>
2022-12-21 13:54:56 +0100

    add owner ad dateend to Service model

--- models/service.js
+++ models/service.js
@@ -1,11 +1,13 @@
 export class Service{
-    constructor(category,imageUri,price,date,description){
+    constructor(category,imageUri,price,date,description,owner,dateEnd){
         this.id = new Date().toString() + Math.random().toString();
         this.category = category;
         this.imageUri = imageUri;
         this.price = price;
         this.date = date; 
         this.description = description; 
+        this.owner = owner;
+        this.dateEnd = dateEnd;
 
     }
 }

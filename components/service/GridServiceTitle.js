4293b9625fc9d05b2522cec13b8ddfefe0802020
michalR <michal1995.1995@interia.pl>
2022-12-21 13:53:15 +0100

    use progress bar

--- components/service/GridServiceTitle.js
+++ components/service/GridServiceTitle.js
@@ -1,12 +1,13 @@
 
 import React from 'react';
-import { FlatList, Pressable, View, Text, StyleSheet, Platform ,useWindowDimensions } from 'react-native';
+import { FlatList, Pressable, View, Text, StyleSheet, Platform ,useWindowDimensions,Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import * as Progress from 'react-native-progress';
 
 // const { height, width } = useWindowDimensions();
 
 
-function GridServiceTitle({ title, color, onPress }) {
+function GridServiceTitle({ title, color, onPress, order}) {
     const navigation = useNavigation();
     const { height, width } = useWindowDimensions();
     return (
@@ -16,13 +17,20 @@ function GridServiceTitle({ title, color, onPress }) {
                 style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}
                 onPress={onPress}
             >
-                <View style={[styles.innerContainer, { backgroundColor: color },{width:width}]}
+                 <View style={[styles.item, { backgroundColor: color },{width:width}]}
 
                 >
-                <View style={styles.innerTextContainer}>
+                {/* <View style={styles.innerTextContainer}>
                     <Text style={styles.textCategory}> {title}</Text>
+                </View> */}
+                
+                 <Image style={styles.image} source={{ uri: order.imageUri }} />
+            <View style={styles.info}>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.address}>{order.date}</Text>
+                <Progress.Bar progress={0.3}  />
                 </View>
-                </View>
+            </View>
             </Pressable>
         </View>
     )
@@ -67,6 +75,18 @@ const styles = StyleSheet.create({
         textAlign: 'center',
 
     },
+    item: {
+        flexDirection: "row",
+        alignItems: 'flex-start',
+        borderRadius: 6,
+        marginVertical: 12,
+        backgroundColor: 'white',
+        // elevation: 2,
+        // shadowColor: 'black',
+        // shadowOpacity: 0.15,
+        // shadowOffset: { width: 1, height: 1 },
+        // shadowRadius: 2
+    },
     innerTextContainer:{
         justifyContent: 'center',
         alignItems: 'center',
@@ -77,5 +97,21 @@ const styles = StyleSheet.create({
     },
     button: {
         flex: 1
-    }
+    },
+    image: {
+        flex: 1,
+        borderBottomLeftRadius: 4,
+        borderTopLeftRadius: 4,
+        height: 100
+    },
+    info: {
+        flex: 2,
+        padding:12
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: "gray"
+    },
+    address: {fontSize:12,color: "gray"}
 })

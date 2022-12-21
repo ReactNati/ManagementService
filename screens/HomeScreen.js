5f2d2945ab3fa5650ad067cb862733b50684707f
michalR <michal1995.1995@interia.pl>
2022-12-21 13:53:43 +0100

    change homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,69 +1,90 @@
 import axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
-import { Alert, StyleSheet, Text, View,FlatList } from 'react-native';
+import { Alert, StyleSheet, Text, View, FlatList } from 'react-native';
 import GridServiceAdd from '../components/service/GridServiceAdd';
 //import {AuthContext} from '../store/auth-context'
 import GridServiceTitle from '../components/service/GridServiceTitle';
-import {Colors} from '../constants/styles'
-import {useSelector,useDispatch} from 'react-redux'
+import { Colors } from '../constants/styles'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchServices } from '../util/database';
+import { useIsFocused } from '@react-navigation/native';
 
-function HomeScreen({navigation}) {
-  const [fetchMessage,setFetchMessage] = useState('');
-const service = useSelector((state)=> state.service.service) 
-  console.log("service" + JSON.stringify(service))
-  const dataExample =[
-    {id:1,title:"Add service"}
-
-  ]
-// useEffect(()=>{
-//  // const auth = useSelector((state)=> state.auth.token) 
+function HomeScreen({ navigation }) {
+  const [fetchMessage, setFetchMessage] = useState('');
+  const [services, setLoadedServicess] = useState([]);
   
-// })
-  function renderServiceDetails(itemData){
+  const email = useSelector((state) => state.auth.email)
 
-    function pressHandler(){
-      navigation.navigate("ServiceDetails",{
+
+  // const dataExample = [
+  //   { id: 1, title: "Add service" }
+
+  // ]
+  // useEffect(()=>{
+  //  // const auth = useSelector((state)=> state.auth.token) 
+
+  // })
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+
+    async function loadPlaces() {
+      // const servicee = useSelector((state) => state.auth.email)
+      // console.log("service" + JSON.stringify(servicee))
+      const services = await fetchServices(email);
+      setLoadedServicess(services);
+    }
+    if (isFocused) {
+      loadPlaces();
+      //  setLoadedPlaces((curPlaces)=>[...curPlaces,route.params.place])
+    }
+  }, [isFocused])
+
+  function renderServiceDetails(itemData) {
+    console.log(itemData)
+    function pressHandler() {
+      navigation.navigate("ServiceDetails", {
         serviceId: itemData.item.id
 
       })
 
     }
 
-      return (
-        
-      <GridServiceTitle title={itemData.item.title} onPress={pressHandler}></GridServiceTitle>
-      
-      )
+    return (
+
+      <GridServiceTitle title={itemData.item.category} onPress={pressHandler} order={itemData.item}></GridServiceTitle>
+
+    )
   }
 
-  function navigateToManageServiceService(){
+  function navigateToManageServiceService() {
     navigation.navigate("ManageService");
   }
   //const authCtx = useContext(AuthContext);
   //const token = authCtx.token;
-//   useEffect(()=>{
-//     try{
-//     axios.get("https://reactnativecours-default-rtdb.firebaseio.com/message.json?auth=" + token).then((response)=>{
-//     console.log(response.data)
-//     setFetchMessage(response.data)
-//     })
-//   } catch(error){
-//     Alert.alert(error)
-//   }
-// },[token])
+  //   useEffect(()=>{
+  //     try{
+  //     axios.get("https://reactnativecours-default-rtdb.firebaseio.com/message.json?auth=" + token).then((response)=>{
+  //     console.log(response.data)
+  //     setFetchMessage(response.data)
+  //     })
+  //   } catch(error){
+  //     Alert.alert(error)
+  //   }
+  // },[token])
 
   return <View style={styles.rootContainer}>
-      <Text>{fetchMessage}</Text>
-      <GridServiceAdd title={"Add Service"} colorIcon={Colors.primary700} icon={'pluscircleo'} size={44} onPress={navigateToManageServiceService} ></GridServiceAdd>
-      <FlatList
-      data={dataExample}
-      keyExtractor={dataExample.id}
+    <Text>{fetchMessage}</Text>
+    <GridServiceAdd title={"Add Service"} colorIcon={Colors.primary700} icon={'pluscircleo'} size={44} onPress={navigateToManageServiceService} ></GridServiceAdd>
+    <FlatList
+      data={services}
+      keyExtractor={(item) => item.id}
       renderItem={renderServiceDetails}
       numColumns={1}
       horizontal={false}
-      />
-    </View>
-  
+    />
+  </View>
+
 }
 
 export default HomeScreen;
